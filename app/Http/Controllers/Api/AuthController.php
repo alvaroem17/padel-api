fcd100a9bde89d55a10a0d701d102ea4e387db1e
Alvaro Estevez Muñoz <alvaro5estevez@gmail.com>
2026-03-17 09:18:15 +0000

    Login and logout done

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    //TODO: Add dtos and response resources for better structure and maintainability
     public function register(Request $request)
     {
         $validatedData = $request->validate([
@@ -29,6 +30,18 @@ class AuthController extends Controller
     }
 
     public function login(Request $request){
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response()->json(['message' => 'Invalid login credentials'], 401);
+        }
 
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['user' => $user, 'token' => $token], 200);
+    }
+
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+        return response()->json(['message' => 'Logged out successfully'], 200);
     }
 }
